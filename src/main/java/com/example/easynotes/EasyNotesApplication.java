42a00ad53654d7481de75f9d9ebacf8f81e4a675
hakoemmy <hakoemmy.2017@gmail.com>
2021-08-25 18:28:41 +0200

    add CRUD notes

---
+++ src/main/java/com/example/easynotes/EasyNotesApplication.java
@@ -0,0 +1,17 @@
+package com.example.easynotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableJpaRepositories
+public class EasyNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyNotesApplication.class, args);
+	}
+
+}
